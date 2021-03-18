e4a16f15f9fa45e3763b3ea381d4fdb19bd2d4bb
juppy10 <william.jupp@gmail.com>
2021-03-18 19:56:23 +0800

    init commit

---
+++ code/cmake-build-debug/CMakeFiles/code.elf.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Startup/startup_stm32f103tbux.s" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Startup/startup_stm32f103tbux.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/main.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/main.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/stm32f1xx_hal_msp.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/stm32f1xx_it.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/syscalls.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/sysmem.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/sysmem.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Core/Src/system_stm32f1xx.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "D:/Will/Documents/GitHub/LV-BMS/code/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "D:/Will/Documents/GitHub/LV-BMS/code/cmake-build-debug/CMakeFiles/code.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
