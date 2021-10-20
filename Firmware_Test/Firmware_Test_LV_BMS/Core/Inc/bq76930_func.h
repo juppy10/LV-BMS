374094117090b73e05369705f87861b658c782dd
juppy10 <william.jupp@gmail.com>
2021-10-20 10:41:15 +0800

    Improved

---
+++ Firmware_Test/Firmware_Test_LV_BMS/Core/Inc/bq76930_func.h
@@ -0,0 +1,39 @@
+//
+// Created by yaboi on 8/04/2021.
+//
+
+#ifndef CODE_BQ76930_FUNC_H
+#define CODE_BQ76930_FUNC_H
+
+#define shunt_mOhm 4
+#define deltaT_ms 250  //time difference between CC calculations in ms
+#define max_temp_degC 60
+#define OC_C_limit_A 10 //charge limit
+#define OC_D_limit_A 30 //discharge limit - double check
+#define UV_limit_mV 3000
+#define OV_limit_mV 4200
+#define BALANCE_THRES_mV 20 //20mV
+#define NTCBeta 3970    //thermistor beta value
+
+//battery protection limit settings
+void setTemperatureLimits(int max_degC);
+long setShortCircuitProtection(long current_mA);
+long setOvercurrentDischargeProtection(long current_mA);
+int setCellUndervoltageProtection(int voltage_mV, int delay_s);
+int setCellOvervoltageProtection(int voltage_mV, int delay_s);
+
+uint8_t getGain();
+
+//battery status
+long  getBatteryCurrent(void);
+int  getBatteryVoltagemV(void);
+int  updateCellVoltages();
+int  getCellVoltage_mV(int idCell);
+int  getMinCellVoltage(void);
+int  getMaxCellVoltage(void);
+float getTemperatureDegC(int isThermistor);
+void updateCC(void);
+void balance_charge(void);
+
+
+#endif //CODE_BQ76930_FUNC_H
--- code/Core/Src/registers.c
+++ code/Core/Src/registers.c
@@ -5,11 +5,11 @@
 #include "main.c"
 
 void writeRegister(uint8_t address, uint8_t data){
-    uint8_t buf[3];        //is it meant to be uint or char??
+    uint8_t buf[3];        
     HAL_StatusTypeDef I2CStat;
     buf[0] = address; buf[1] = data;
 
-    I2CStat = HAL_I2C_Master_Transmit(&hi2c1,BQ_I2CADDRESS,buf,2,HAL_MAX_DELAY);
+    I2CStat = HAL_I2C_Mem_Write(&hi2c1, BQ_I2CADDRESS, buf[0], 1, buf[1], 1, HAL_MAX_DELAY);
     if (I2CStat!=HAL_OK){
         //Error?
         //Try again?
@@ -17,13 +17,12 @@ void writeRegister(uint8_t address, uint8_t data){
     //Add CRC later
 }
 
-int readRegister(uint8_t address){
+uint8_t readRegister(uint8_t address){
     uint8_t buf[1];
     HAL_StatusTypeDef I2CStat;
 
     buf[0] = address;
-    HAL_I2C_Master_Transmit(&hi2c1,BQ_I2CADDRESS,buf,1,HAL_MAX_DELAY);
-    I2CStat = HAL_I2C_Master_Receive(&hi2c1,BQ_I2CADDRESS,buf,1,HAL_MAX_DELAY);
+    I2CStat = HAL_I2C_Mem_Read(&hi2c1,BQ_I2CADDRESS,buf[0],1,buf[0],1,HAL_MAX_DELAY);
     if (I2CStat!=HAL_OK){
         //Error?
         //Try again?
@@ -31,19 +30,18 @@ int readRegister(uint8_t address){
     return buf[0];
 }
 
-int readRegister_2(uint8_t address){    //needs to be checked, for I2C auto increment
+
+//Takes address of first register 
+//Returns 16-bit int where 4 MSB are first register and 4LSB are second register
+uint16_t readRegister_2(uint8_t address){    //needs to be checked, for I2C auto increment
     uint8_t buf[2];
-    int doubleData;
     HAL_StatusTypeDef I2CStat;
 
     buf[0] = address;
-    HAL_I2C_Master_Transmit(&hi2c1,BQ_I2CADDRESS,buf,1,HAL_MAX_DELAY);
-    I2CStat = HAL_I2C_Master_Receive(&hi2c1,BQ_I2CADDRESS,buf,2,HAL_MAX_DELAY);
+    I2CStat = HAL_I2C_Mem_Read(&hi2c1,BQ_I2CADDRESS,buf[0],1,buf[0],2,HAL_MAX_DELAY);
     if (I2CStat!=HAL_OK){
         //Error?
         //Try again?
     }
-    doubleData = (buf[0]<<8)|(buf[1]);  //concatenate the 2 bytes of data
-
-    return doubleData;
+    return (buf[0]<<8) | (buf[1] & 0xff);
 }
